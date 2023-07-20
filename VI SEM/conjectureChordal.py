5726c70bd48c7dedf54af3607b002cc995983641
jp-karthik <karthikjagadeeshponarkar@gmail.com>
2023-07-20 19:08:38 +0530

    Pushing in some required changes

--- VI SEM/conjectureChordal.py
+++ VI SEM/conjectureChordal.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import itertools
 
 import sys
 sys.path.append("/Users/karthikjp/Desktop/MNS-vs-MCS/VI SEM/graphSearch") # add the parent directory to the path
@@ -107,6 +108,119 @@ def conjecture(V:int, edges:list[list]) -> bool:
     return False
 
 
+# visit all the vertices in a clique, for each vertex which are not yet visited if it is maximal
+def check3(V:int, edges:list[list]) -> bool:
+    G = nx.Graph()
+    nodes = []
+    for i in range(V):
+        nodes.append(i)
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+    adjacentList = [[] for i in range(V)]
+    for edge in edges:
+        adjacentList[edge[0]].append(edge[1])
+        adjacentList[edge[1]].append(edge[0])
+    maximalCliques = find_cliques(G)
+    for clique in maximalCliques:
+        labels = [[] for i in range(V)]        
+        for vk in clique:
+            for ng in adjacentList[vk]:
+                if ng not in clique:
+                    labels[ng].append(vk)
+        maxcard = 0
+        for i in range(V):
+            if i not in clique:
+                maxcard = max(maxcard, len(labels[i]))
+        maximumVertices = []
+        for i in range(V):
+            if i not in clique and len(labels[i]) == maxcard:
+                maximumVertices.append(i)
+        # get all permutations of size three from maximumVertices using itertools
+        for i in range(len(maximumVertices)):
+            for j in range(len(maximumVertices)):
+                for k in range(len(maximumVertices)):
+                    if i != j and j != k:
+                        # visting v and ensuring that (v, u) is not in E and (v, w) is in E and lab(u)\lab(w) != {}
+                        v1 = maximumVertices[i] # u
+                        v2 = maximumVertices[j] # v
+                        v3 = maximumVertices[k] # w
+                        if v1 not in adjacentList[v2] and v3 in adjacentList[v2]:
+                            uMaximalSet = False
+                            for lab in labels[v1]:
+                                if lab not in labels[v3]:
+                                    uMaximalSet = True
+                                    break
+                            if uMaximalSet == False:
+                                continue
+                            else:
+                                return True
+    return False
+    
+def check4(V:int, edges:list[list]) -> bool:
+    G = nx.Graph()
+    nodes = []
+    for i in range(V):
+        nodes.append(i)
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+    adjacentList = [[] for i in range(V)]
+    for edge in edges:
+        adjacentList[edge[0]].append(edge[1])
+        adjacentList[edge[1]].append(edge[0])
+    maximalCliques = find_cliques(G)
+    for clique in maximalCliques:
+
+        labels = [[] for i in range(V)]        
+        for vk in clique:
+            for ng in adjacentList[vk]:
+                if ng not in clique:
+                    labels[ng].append(vk)
+
+        onlyAdjToClique = []
+        
+        for v in range(V):
+            if v not in clique:
+                b = True
+                for ng in adjacentList[v]:
+                    if ng not in clique:
+                        b = False
+                        break
+                if b:
+                    onlyAdjToClique.append(v)
+
+        for ATCV in onlyAdjToClique:
+
+            maxcard = 0
+            for i in range(V):
+                if i not in clique and i != ATCV:
+                    maxcard = max(maxcard, len(labels[i]))
+            maximumVertices = []
+
+            for i in range(V):
+                if i not in clique and i != ATCV and len(labels[i]) == maxcard:
+                    maximumVertices.append(i)
+
+            # get all permutations of size three from maximumVertices using itertools
+            for i in range(len(maximumVertices)):
+                for j in range(len(maximumVertices)):
+                    for k in range(len(maximumVertices)):
+                        if i != j and j != k:
+                            # visting v and ensuring that (v, u) is not in E and (v, w) is in E and lab(u)\lab(w) != {}
+                            v1 = maximumVertices[i] # u
+                            v2 = maximumVertices[j] # v
+                            v3 = maximumVertices[k] # w
+                            if v1 not in adjacentList[v2] and v3 in adjacentList[v2]:
+                                uMaximalSet = False
+                                for lab in labels[v1]:
+                                    if lab not in labels[v3]:
+                                        uMaximalSet = True
+                                        break
+                                if uMaximalSet == False:
+                                    continue
+                                else:
+                                    return True
+    return False
+
 def getInput(filename):
 
     fp = open(filename, "r")
@@ -125,6 +239,7 @@ def getInput(filename):
             s = fp.readline().strip().split(" ")
             V, E = int(s[0]), int(s[1])
             edges = []
+
             for j in range(E):
                 t = fp.readline().strip().split(" ")
                 t[0], t[1] = int(t[0]), int(t[1])
@@ -138,12 +253,26 @@ def getInput(filename):
                 if E > V-1:
                     cntNIG += 1
 
+            if IG.isInteresting(V, E, edges) == False and (basic(V, edges) == True or conjecture(V, edges) == True or check3(V, edges) == True):
+                print("High alert")
+
+            CND = basic(V, edges) or conjecture(V, edges) or check3(V, edges) or check4(V, edges)
+
+            if CND == False and IG.isInteresting(V, E, edges) == True:
+                print("to be convered")
+                print(edges)
+
             if basic(V, edges) == True:
                 continue
 
+            if check3(V, edges) == True:
+                continue
+
+            if check4(V, edges) == True:
+                continue
+
             if conjecture(V, edges) == False and E > V-1:
-                print("alert", i)
-                cnt += 1
+                cnt += 1                   
 
         print()
         print("Total number of nonIntersting graphs obs:", cnt)
